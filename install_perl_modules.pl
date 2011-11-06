cbba9d2e342947ab116318ec628b181e13262bcb
Stefan Profanter <git@s.profanter.me>
2011-11-06 23:35:06 +0100

    Added Admin check under windows

--- install_perl_modules.pl
+++ install_perl_modules.pl
@@ -1,9 +1,21 @@
 #!/usr/bin/env perl
-use CPAN::Shell;
+use CPAN;
 
 if($^O !~ /Win32/i and getpwuid($<) ne 'root'){
   print "Please run the module installer as root!\n";
   exit();
+} elsif($^O =~ /Win32/i) {
+	my $retval = system('mkdir "%windir%\system32\putio_sync_test') >> 8;
+
+	if ($retval == 0)
+	{
+		system('rmdir "%windir%\system32\putio_sync_test')
+	}
+	else
+	{
+		print "Please run the module installer as administrator!\n";
+		exit();
+	}
 }
 
 my @requiredModules = (
