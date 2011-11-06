f806be8ad95ee2529510d963cc2653c2eee9a243
Matthias Schwab <matthias@matthiasschwab.de>
2011-11-06 23:04:50 +0100

    Changed module installer script

---
+++ install_perl_modules.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/env perl
+use CPAN::Shell;
+
+if($^O !~ /Win32/i and getpwuid($<) ne 'root'){
+  print "Please run the module installer as root!\n";
+  exit();
+}
+
+my @requiredModules = (
+  'WebService::PutIo::Files',
+  'Getopt::Long',
+  'Data::Dumper',
+  'LWP::UserAgent',
+  'LWP::Protocol::https',
+  'XML::Simple',
+  'File::Path',
+  'TVDB::API'
+);
+
+for my $moduleName (@requiredModules){
+  printf("Installing '%s'...\n", $moduleName);
+  CPAN::Shell->install($moduleName);
+}
