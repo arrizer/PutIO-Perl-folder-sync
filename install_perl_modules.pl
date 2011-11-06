7ab505dd7673d32308e21b073a118693930a79bf
Matthias Schwab <matthias@matthiasschwab.de>
2011-11-06 23:04:50 +0100

    Changed module installer script

---
+++ install_perl_modules.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/env perl
+use CPAN::Shell;
+
+my $username = getpwuid( $< );
+
+if($^O !~ /Win32/i and $username ne 'root'){
+  print "Please run the module installer as root!\n";
+  exit();
+}
+
+my @requiredModules = (
+  'WebService::PutIo::Files',
+  'Getopt::Long',
+  'Data::Dumper',
+  'LWP::UserAgent',
+  'LWP::Protocol::https',
+  'XML::Simple',
+  'File::Path',
+  'TVDB::API'
+);
+
+for my $moduleName (@requiredModules){
+  printf("Installing '%s'...\n", $moduleName);
+  CPAN::Shell->install($moduleName);
+}
--- install_perl_modules.sh
+++
@@ -1,9 +0,0 @@
-echo "This will install or update the required perl modules for putiosync..."
-sudo perl -MCPAN -e 'install WebService::PutIo::Files'
-sudo perl -MCPAN -e 'install Getopt::Long'
-sudo perl -MCPAN -e 'install Data::Dumper'
-sudo perl -MCPAN -e 'install LWP::UserAgent'
-sudo perl -MCPAN -e 'install LWP::Protocol::https'
-sudo perl -MCPAN -e 'install XML::Simple'
-sudo perl -MCPAN -e 'install File::Path'
-sudo perl -MCPAN -e 'install TVDB::API'
